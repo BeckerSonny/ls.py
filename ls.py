a73050f4aefc9b31ec5ddae0e681de908d3e57b3
sonny <sonny@DESKTOP-QON9Q2B.localdomain>
2019-11-17 15:18:04 +0100

    Add comment and a better gestion for argv

--- ls.py
+++ ls.py
@@ -4,17 +4,22 @@ import datetime
 
 def ls():
     print("Args ==> ", sys.argv)
+    # Options get all optionnal parameters like "-l"
     options = []
-    path = None
     for argv in sys.argv:
         if argv == __file__:
             continue
+        # Check if an Argv is an optionnal paramater (take only "-l", else it's unknow)
         if argv[:1] == "-":
             if argv == "-l":
                 options.append(argv)
+            else:
+                print("Unknow parameter {}.".format(argv))
         else:
+            # If the agrv is not the file name or an optionnal parameter, it is considered like a path
+            # Launch function for get file
             return find_files_in_path(argv, options)
-    return "No path found"
+    return "No path found in argv"
 
 
 
